79d2aa37152d742fc2c4fd57aa8ec606e81dc209
Bruno Carvalho <bdo@vizrt.com>
2023-12-06 17:33:48 +0000

    add bruteforce for day 6

--- aoc_2023_6.py
+++ aoc_2023_6.py
@@ -1,3 +1,5 @@
+import time as T
+start = T.time()
 lines = open('input.txt', 'r').read().splitlines()
 ts = lines[0].split(":")[1].split()
 ds = lines[1].split(":")[1].split()
@@ -5,6 +7,7 @@ times =  [int(n) for n in ts]
 distances = [int(n) for n in ds]
 times.append(int(''.join(ts)))
 distances.append(int(''.join(ds)))
+#quadratic solve solution
 total1 = 1
 total2 = 0
 for i, time in enumerate(times):
@@ -13,9 +16,28 @@ for i, time in enumerate(times):
     rec = abs(int(u2)-int(u1))  
     if i != len(times)-1:
         total1 *= rec
-    total2 = rec
+    else:
+        total2 = rec
 print(total1)
 print(total2)
+print(f'Elapsed {(T.time()-start)*1000} ms')
+#brute force solution
+start = T.time()
+total1 = 1
+total2 = 0
+for i, time in enumerate(times):
+    rec = 0
+    for k in range(time):
+        if k*(time-k) > distances[i]:
+            rec += 1
+    if i != len(times)-1:
+        total1 *= rec
+    else:
+        total2 = rec
+print(total1)
+print(total2)
+print(f'Elapsed {(T.time()-start)*1000} ms')
+
 
 
 
