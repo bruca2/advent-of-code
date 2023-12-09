6aade9a4c5bc6dec21e1417d1f6092cdc550b965
Bruno Carvalho <bdo@vizrt.com>
2023-12-09 15:38:18 +0000

    add solve time graph

--- aoc_2023_all.py
+++ aoc_2023_all.py
@@ -2,15 +2,27 @@ import os
 import os.path
 import time as T
 import aoc_2023
+import matplotlib.pyplot as plt
 
 class t:
     def startTime(self):
         self.start = T.time()        
     
     def printTime(self):
-        print(f'Elapsed {(T.time()-self.start)*1000:.0f} ms')
+        elapsed = (T.time()-self.start)*1000
+        print(f'Elapsed {elapsed:.0f} ms')
+        return elapsed
+    
+    def plotTime(self, T):
+        X = range(1,len(aoc_2023.__all__)+1)
+        plt.xlabel('Problem number')
+        plt.ylabel('Time (ms)')
+        plt.title('AOC 2023 solving times')
+        plt.scatter(X,T)
+        plt.show()
 
 days = map(aoc_2023.__dict__.get, aoc_2023.__all__)
+times = []
 
 cl = t()
 cl.startTime()
@@ -21,8 +33,10 @@ for day in days:
     print(f'Day {day.__name__}')
     os.chdir(os.path.dirname(day.__file__))
     day.run()
-    cd.printTime()
+    elapsed = cd.printTime()
     print('========================')
+    times.append(elapsed)
     
 print('Total:',end='')
 cl.printTime()
+cl.plotTime(times)
